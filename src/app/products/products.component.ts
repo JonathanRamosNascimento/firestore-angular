fd5099e2f82ad0761f25c5a765c67f5768cf77cb
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-22 00:16:11 -0300

    creating product add form methods

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,8 @@
+import { Product } from './../models/product.model';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  productForm = this.fb.group({
+    id: [undefined],
+    name: ['', [Validators.required]],
+    stock: [0, [Validators.required]],
+    price: [0, [Validators.required]],
+  })
+
+  constructor(
+    private fb: FormBuilder,
+    private productService: ProductService,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    let p: Product = this.productForm.value;
+
+    if (!p.id) {
+      this.addProduct(p);
+    } else {
+      this.editProduct(p);
+    }
+  }
+
+  addProduct(p: Product) {
+    this.productService.addProduct(p)
+      .then(() => {
+        this.snackBar.open('Product added.', 'OK', {duration: 2000});
+      })
+      .catch(() => {
+        this.snackBar.open('Error on submit the product.', 'OK', {duration: 2000});
+      })
+  }
+
+  editProduct(p: Product) {
+
+  }
 }
