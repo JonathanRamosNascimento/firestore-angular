e279ca3023fce6161ca035a5d59a0dabfda2a5b0
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-22 01:41:32 -0300

    using environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDNOtXkS1rmnTNFsy_JBXausiWSI4fs_aI",
+    authDomain: "products-prod-349ee.firebaseapp.com",
+    databaseURL: "https://products-prod-349ee.firebaseio.com",
+    projectId: "products-prod-349ee",
+    storageBucket: "",
+    messagingSenderId: "510690313465",
+    appId: "1:510690313465:web:a95a2f3fbfa5426d"
+  }
 };
