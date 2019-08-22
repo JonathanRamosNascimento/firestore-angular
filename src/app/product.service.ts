70f4d072286d022d77e5f9debb55af6dc2dc9242
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-21 23:57:51 -0300

    creating methods of add new product and get products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,24 @@
+import { Product } from './models/product.model';
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private productsCollection: AngularFirestoreCollection<Product> = this.afs.collection('products')
+
+  constructor(
+    private afs: AngularFirestore
+  ) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.productsCollection.valueChanges();
+  }
+
+  addProduct(p: Product) {
+    this.productsCollection.add(p);
+  }
 }
